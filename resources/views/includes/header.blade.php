26839b7c88559561bdd3d0595ac085d9127ac60c
francistito <francistito07@gmail.com>
2021-07-12 17:07:48 +0300

    modify font and colors

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -34,7 +34,7 @@
                             <div class="header-nav-main header-nav-main-square header-nav-main-effect-1 header-nav-main-sub-effect-1">
                                 <nav class="collapse">
                                     <ul class="nav nav-pills" id="mainNav">
-                                        <li class="dropdown"><a class="dropdown-item dropdown-toggle" href="{{url('/')}}" style="font-size: 15px"> {{trans('label.home')}} <i class="fas fa-chevron-down"></i></a>
+                                        <li class="dropdown"><a class="dropdown-item dropdown-toggle" href="{{url('/')}}" style="font-size: 14px"> {{trans('label.home')}} <i class="fas fa-chevron-down"></i></a>
 
                                         </li>
 
