700a9c3819e54b5002bf62ec9ad5b2dfa2d7acc5
francistito <francistito07@gmail.com>
2021-07-17 09:16:58 +0300

    try new font

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -22,9 +22,10 @@
 
     <!-- Web Fonts  -->
 
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@200;500&family=Roboto:ital,wght@0,300;1,900&display=swap" rel="stylesheet">
-{{--    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7COpen+Sans:400,700,800" rel="stylesheet" type="text/css">--}}
-
+{{--    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap" rel="stylesheet">--}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@200;500&family=Open+Sans:ital,wght@0,400;1,300&family=Roboto:ital,wght@0,300;1,900&display=swap" rel="stylesheet">
     <!-- Vendor CSS -->
     {{ Html::style(url('vendor/bootstrap/css/bootstrap.min.css')) }}
     {{ Html::style(url('vendor/fontawesome-free/css/all.min.css')) }}
@@ -64,6 +65,20 @@
     <!-- Theme Custom CSS -->
     {{ Html::style(url('css/custom.css')) }}
 
+    <style>
+        /*body{*/
+        /*    font-family: 'Open Sans', sans-serif;*/
+
+        /*    font-style:*/
+        /*    normal*/
+        /*    Weight*/
+        /*    400*/
+        /*    Size*/
+        /*    16px*/
+        /*    Line Height;*/
+
+        /*}*/
+    </style>
 
        @yield('after-styles')
 
