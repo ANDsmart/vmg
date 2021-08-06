f4f03b7669b38051161ba11f74ae07b08b0d71b4
francistito <francistito07@gmail.com>
2021-08-06 16:44:19 +0300

    remove unwanted folder

--- resources/views/cms/user_manual/includes/components/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="simple_footer">
-    <img class="leaf_right" src="img/home_one/leaf_footter.png" alt="">
-    <div class="container ">
-
-    </div>
-</footer>
--- resources/views/system/service/service.blade.php
+++ resources/views/system/service/service.blade.php
@@ -63,6 +63,26 @@
                             <p>{!! $service->content !!}</p>
 
                             @if($service->service_type_cv_id == 8)
+                                <section class="parallax section section-text-light section-parallax" data-plugin-parallax="" data-plugin-options="{'speed': 1.5, 'parallaxHeight': '210%'}" data-image-src="img/parallax/parallax-2.jpg" style="position: relative; overflow: hidden;"><div class="parallax-background" style="background-image: url('img/parallax/parallax-2.jpg&quot;); background-size: cover; position: absolute; top: 0px; left: 0px; width: 100%; height: 210%; transform: translate3d(0px, -117.196px, 0px); background-position-x: 50%;"></div>
+                                    <section class="call-to-action">
+                                        <div class="container">
+                                            <div class="row">
+                                                <div class="col-sm-9 col-lg-9">
+                                                    <div class="call-to-action-content">
+                                                        <h3>Porto is <strong class="font-weight-extra-bold">everything</strong> you need to create a <strong class="font-weight-extra-bold">website!</strong></h3>
+                                                        <p class="mb-0">The <strong class="font-weight-extra-bold">Best</strong> HTML Site Template on ThemeForest</p>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-3 col-lg-3">
+                                                    <div class="call-to-action-btn">
+                                                        <a href="http://themeforest.net/item/porto-responsive-html5-template/4106987" target="_blank" class="btn btn-modern text-2 btn-primary">Buy Now</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </section>
+                                </section>
+
                             <div class="row">
                                 <div class="col">
                                     <div class="overflow-hidden mb-3">
