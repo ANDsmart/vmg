ab38703153e70c8beed7957f07359954cf998482
francistito <francistito07@gmail.com>
2022-02-22 11:06:08 +0300

    changes on about us page

--- resources/views/system/general_information/about_us.blade.php
+++ resources/views/system/general_information/about_us.blade.php
@@ -18,7 +18,7 @@
 @endpush
 
 @section('content')
-        <section class="page-header page-header-modern page-header-background page-header-background-md overlay overlay-color-dark overlay-show overlay-op-7" style="background-image:url( {{url('img/page-header/page-header-services.jpg')}});">
+        <section class="page-header page-header-modern page-header-background page-header-background-md overlay overlay-color-dark overlay-show overlay-op-7" style="background-image:url( {{url('img/page-header/our-office-1.jpg')}});">
             <div class="container">
                 <div class="row mt-5">
                     <div class="col-md-12 align-self-center p-static order-2 text-center">
@@ -122,7 +122,7 @@
         <section class="section section-height-3 bg-color-grey-scale-1 m-0 border-0">
             <div class="container">
                 <div class="row align-items-center justify-content-center">
-                    <div class="col-lg-12 pb-sm-4 pb-lg-0 pr-lg-5 mb-sm-5 mb-lg-0">
+                    <div class="col-lg-6 pb-sm-4 pb-lg-0 pr-lg-5 mb-sm-5 mb-lg-0">
                         <h2 class="text-color-dark font-weight-normal text-6 mb-2">Who <strong class="font-weight-extra-bold">We Are</strong></h2>
                         <p><strong>VMG </strong> is a set of activities including
                             planning and decision making, organizing, leading and controlling directed at an organization`s human,
@@ -140,9 +140,9 @@
                         <p align="right">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <em><strong>VIPAWA MANAGEMENT GROUP&nbsp;</strong> </em></p>
                     </div>
                     <div class="col-sm-8 col-md-6 col-lg-4 offset-sm-4 offset-md-4 offset-lg-2 mt-sm-5" style="top: 1.7rem;">
-                        <img src="img/generic/generic-corporate-3-1.jpg" class="img-fluid position-absolute d-none d-sm-block appear-animation animated expandIn appear-animation-visible" data-appear-animation="expandIn" data-appear-animation-delay="300" style="top: 10%; left: -50%; animation-delay: 300ms;" alt="">
-                        <img src="img/generic/generic-corporate-3-2.jpg" class="img-fluid position-absolute d-none d-sm-block appear-animation animated expandIn appear-animation-visible" data-appear-animation="expandIn" style="top: -33%; left: -29%; animation-delay: 100ms;" alt="">
-                        <img src="img/generic/generic-corporate-3-3.jpg" class="img-fluid position-relative appear-animation mb-2 animated expandIn appear-animation-visible" data-appear-animation="expandIn" data-appear-animation-delay="600" alt="" style="animation-delay: 600ms;">
+                        <img src="{{url('img/generic/generic-corporate-3-1.jpg')}}" class="img-fluid position-absolute d-none d-sm-block appear-animation animated expandIn appear-animation-visible" data-appear-animation="expandIn" data-appear-animation-delay="300" style="top: 10%; left: -50%; animation-delay: 300ms;" alt="">
+                        <img src="{{url('img/generic/generic-corporate-3-2.jpg')}}" class="img-fluid position-absolute d-none d-sm-block appear-animation animated expandIn appear-animation-visible" data-appear-animation="expandIn" style="top: -33%; left: -29%; animation-delay: 100ms;" alt="">
+                        <img src="{{url('img/generic/generic-corporate-3-3.jpg')}}" class="img-fluid position-relative appear-animation mb-2 animated expandIn appear-animation-visible" data-appear-animation="expandIn" data-appear-animation-delay="600" alt="" style="animation-delay: 600ms;">
                     </div>
                 </div>
             </div>
