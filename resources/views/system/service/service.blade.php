beb71d7c9e20aed6339f6902a621db067d8bfbe7
francistito <francistito07@gmail.com>
2022-08-01 09:26:00 +0300

    resolve image url

--- resources/views/system/service/service.blade.php
+++ resources/views/system/service/service.blade.php
@@ -138,7 +138,7 @@
                 <div class="col">
                     <div class="row align-items-center pt-4 appear-animation" data-appear-animation="fadeInLeftShorter">
                         <div class="col-md-4 mb-4 mb-md-0">
-                            <img class="img-fluid scale-2 pr-5 pr-md-0 my-4" src="{{url('http://142.93.210.219/img/compl.png')}}" alt="layout styles" />
+                            <img class="img-fluid scale-2 pr-5 pr-md-0 my-4" src="{{url('img/compl.png')}}" alt="layout styles" />
                         </div>
                         <div class="col-md-8 pl-md-5">
                             <h2 class="font-weight-normal text-6 mb-3"><strong class="font-weight-extra-bold">HR Compliance Reviews</strong> </h2>
